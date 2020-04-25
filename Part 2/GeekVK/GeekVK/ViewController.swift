0d12b38b5a0e3fb8d5fbb0f9c1d1ffcefcf67fe1
Anton Danilov <danilovantonne@gmail.com>
2020-04-25 16:12:45 +0300

    base commit

---
+++ Part 2/GeekVK/GeekVK/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GeekVK
+//
+//  Created by Антон Данилов on 25.04.2020.
+//  Copyright © 2020 Антон Данилов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBAction func loginButton(_ sender: UIButton) {
+        print("Login complete")
+    }
+}
+
